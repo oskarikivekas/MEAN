aa0e5fad97ab5c84edfb2e6731e5641d06b84e5e
Chef Kari <oskari.kivekas@student.lut.fi>
2022-11-05 11:03:36 +0200

    Mongoose db connection, added env config file (gitignored), cleanup generated code

--- app.js
+++ app.js
@@ -1,17 +1,29 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const dotenv = require('dotenv')
+const passport = require('passport');
+const mongoose = require('mongoose');
+
+// secrets and other config data stored in env file
+dotenv.config();
+mongoose.connect(process.env.DB_URL, {useNewUrlParser: true, useUnifiedTopology: true});
+const db = mongoose.connection;
+
+db.on('error', (error) => console.log(error))
+db.once('open', () => console.log('Connected to database'));
 
 var app = express();
 
-app.use(logger('dev'));
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+app.use(cors());
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.post('/register', async (req, res, next) => {
+  res.send('Registered')
+})
+
 module.exports = router;
