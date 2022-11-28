66059168044b62c87193f30c614b05ce8901c86c
Chef Kari <oskari.kivekas@student.lut.fi>
2022-11-28 18:55:04 +0200

    Home page and Register page completed + minor fixes and README

--- angular-src/src/app/components/register/register.component.ts
+++ angular-src/src/app/components/register/register.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ValidateService } from 'app/services/validate.service';
+import { FlashMessagesService } from 'angular2-flash-messages';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  name: String;
+  username: String;
+  email: String;
+  password: String;
 
-  constructor() { }
+  constructor(private ValidateService: ValidateService, private flashMsg: FlashMessagesService) { }
 
   ngOnInit() {
   }
 
+  onSubmit(){
+    const user = {
+      name: this.name,
+      username: this.username,
+      email: this.email,
+      password: this.password
+    }
+    // Check for empty fields
+    if (!this.ValidateService.validateRegister(user)) {
+      this.flashMsg.show("Fill in ALL required fields", {cssClass: 'alert-warning', timeout: 3000});
+      return false;
+    }
+    // check for email format
+    if (!this.ValidateService.validateEmail(user.email)) {
+      this.flashMsg.show("Check email format", {cssClass: 'alert-warning', timeout: 3000});
+      return false;
+    }
+  }
 }
